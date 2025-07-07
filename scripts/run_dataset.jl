924326fbc76cd1e3cd61258a8391375eb095124f
Kaihuang Chen <kaihuang.chen@connect.polyu.hk>
2025-07-07 17:22:20 +0800

    chang some notations

--- scripts/run_dataset.jl
+++ scripts/run_dataset.jl
@@ -8,7 +8,7 @@ params.max_iter = typemax(Int32)
 params.time_limit = 3600
 params.stoptol = 1e-8
 params.device_number = 0
-params.warm_up = false
+params.warm_up = true
 params.use_gpu = true
 
 HPRLP.run_dataset(data_path, result_path, params)
